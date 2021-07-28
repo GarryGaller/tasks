3dc3659e7182cc2671ac1620285dae3eef14196f
GarryGaller <ggaller@mail.ru>
2021-07-28 14:24:58 +0300

    Added comment

--- ozon/max_subarray_problem.go
+++ ozon/max_subarray_problem.go
@@ -1,41 +1,42 @@
 package ozon
 
 import (
-	"fmt"
+    "fmt"
 )
 
 func SlicesEqual(a, b []int) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	for i := range a {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-	return true
+    if len(a) != len(b) {
+        return false
+    }
+    for i := range a {
+        if a[i] != b[i] {
+            return false
+        }
+    }
+    return true
 }
 
 func FindSubarrayWithMaxSum(arr []int) [2]int {
-	startSum := arr[0]
-	leftBound, rightBound, partialSum := 0, 0, 0
-	minusPos := -1
-	n := len(arr)
+    /* Kadane's algorithm */
+    bestSum := arr[0]
+    leftBound, rightBound, partialSum := 0, 0, 0
+    minusPos := -1
+    n := len(arr)
 
-	for r := 0; r < n; r++ {
-		partialSum += arr[r]
+    for r := 0; r < n; r++ {
+        partialSum += arr[r]
 
-		if partialSum > startSum {
-			startSum = partialSum
-			leftBound = minusPos + 1
-			rightBound = r
-		}
+        if partialSum > bestSum {
+            bestSum = partialSum
+            leftBound = minusPos + 1
+            rightBound = r
+        }
 
-		if partialSum < 0 {
-			partialSum = 0
-			minusPos = r
-		}
-	}
-	fmt.Println(startSum)
-	return [2]int{leftBound, rightBound}
+        if partialSum < 0 {
+            partialSum = 0
+            minusPos = r
+        }
+    }
+    fmt.Println(bestSum)
+    return [2]int{leftBound, rightBound}
 }
--- ozon/max_subarray_problem_test.go
+++ ozon/max_subarray_problem_test.go
@@ -1,32 +1,35 @@
 package ozon
 
 import (
-	"testing"
+    "testing"
 )
 
 type TestPairs struct {
-	values   []int
-	subarray [2]int
-	sum      float64
+    values   []int
+    subarray [2]int
+    sum      float64
 }
 
 var testsPairs = []TestPairs{
-	{[]int{10, -3, -12, 8, 42, 1, -7, 0, 3}, [2]int{3, 5}, 51},
-	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, [2]int{0, 8}, 45},
-	{[]int{-1, 1, 2, 3, 4, 5, 6, 7, 8, 9}, [2]int{1, 9}, 45},
-	{[]int{10, -3, 12, 8, 42, 1, -7, 0, 3}, [2]int{0, 5}, 70},
-	{[]int{-7, 7, 12, 8, 42, 1, -7, 0, 3}, [2]int{1, 5}, 70},
+    {[]int{10, -3, -12, 8, 42, 1, -7, 0, 3}, [2]int{3, 5}, 51},
+    {[]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, [2]int{0, 8}, 45},
+    {[]int{-1, 1, 2, 3, 4, 5, 6, 7, 8, 9}, [2]int{1, 9}, 45},
+    {[]int{10, -3, 12, 8, 42, 1, -7, 0, 3}, [2]int{0, 5}, 70},
+    {[]int{-7, 7, 12, 8, 42, 1, -7, 0, 3}, [2]int{1, 5}, 70},
+    {[]int{1,2,3,4,5,-5}, [2]int{0, 4}, 15},
+    {[]int{-1,-2,-3}, [2]int{0, 0}, -1},      // if the array contains only negative numbers, the maximum subarray would be the largest element itself. 
+    {[]int{-3,-2,-1}, [2]int{2, 2}, -1},     //  if the array contains only negative numbers, the maximum subarray would be the largest element itself. 
 }
 
 func TestSubarray(t *testing.T) {
-	for _, pair := range testsPairs {
-		v := FindSubarrayWithMaxSum(pair.values)
-		if v != pair.subarray {
-			t.Error(
-				"For", pair.values,
-				"expected", pair.subarray,
-				"got", v,
-			)
-		}
-	}
+    for _, pair := range testsPairs {
+        v := FindSubarrayWithMaxSum(pair.values)
+        if v != pair.subarray {
+            t.Error(
+                "For", pair.values,
+                "expected", pair.subarray,
+                "got", v,
+            )
+        }
+    }
 }
